0cb8a84401f11c4453072182e14f75118bae3b56
Kingsley Ihemere <dekings44@gmail.com>
2022-06-02 07:20:57 +0100

    updated

--- leap_year.py
+++ leap_year.py
@@ -1,16 +1,26 @@
 print('Welcome to a leap year calculator!!!')
 
-year = int(input('Please enter a year. eg. 2020\n'))
 
 
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print(f'Yay!! {year} is a leap year')
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
         else:
-            print(f'{year} did not satisfy condition 3 for a leap year')
+            return True
     else:
-        print(f'{year} is a leap year')
-else:
-    print(f'{year} is not a leap year')
+       return False
+
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) and month == 2:
+        return 29
+    return month_days[month - 1]
+
+year = int(input('Please enter a year. \n'))
+month = int(input('Please enter a month. \n'))
+days = days_in_month(year, month)
 
